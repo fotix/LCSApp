b1bf1792a0ee0fe412a593fe782d5bc282878480
fotix <k4560071>
2015-06-10 16:30:49 +0100

    End point change , new champion request

---
+++ app/src/main/java/com/twistedsin/app/api/models/Game/Champion.java
@@ -0,0 +1,7 @@
+package com.twistedsin.app.api.models.Game;
+
+/**
+ * Created by Filipe on 6/10/2015.
+ */
+public class Champion {
+}
---
+++ app/src/main/java/com/twistedsin/app/api/responses/ChampionResponseNotification.java
@@ -0,0 +1,7 @@
+package com.twistedsin.app.api.responses;
+
+/**
+ * Created by Filipe on 6/10/2015.
+ */
+public class ChampionResponseNotification {
+}
